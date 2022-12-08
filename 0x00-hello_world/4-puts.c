141b87f5dff8e375514a4b59c75b79e678a5f7a2
Shanqs74 <abubakarss@yahoo.com>
2022-12-08 03:30:27 -0800

    Betty is saucy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -Print a line of code using puts
+ * main - Print a line of code using puts
  *
- * return: 0
+ * Return: 0
 */
 
-main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
